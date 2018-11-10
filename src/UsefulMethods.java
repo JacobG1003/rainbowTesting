f8d6d6dad9924d137c69b40ecb2f78a48cd30b13
robosapiens5699 <fhcirobotics@gmail.com>
2018-11-10 11:35:00 -0500

    initial commit

---
+++ src/UsefulMethods.java
@@ -0,0 +1,12 @@
+public class UsefulMethods {
+    /**
+     * adds up two numbers
+     *
+     * @param num1 first number
+     * @param num2 second number
+     * @return sum of num1 and num2
+     */
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
